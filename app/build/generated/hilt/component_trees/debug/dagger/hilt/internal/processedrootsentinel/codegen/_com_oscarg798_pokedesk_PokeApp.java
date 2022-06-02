9f9a8ddb36e4d5e26951dd0de34872516c1ac8c3
Oscar Gallon <oscar.gallon@ocado.com>
2022-06-02 11:40:13 +0200

    fixing git ignore

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_oscarg798_pokedesk_PokeApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.oscarg798.pokedesk.PokeApp"
-)
-public final class _com_oscarg798_pokedesk_PokeApp {
-}
